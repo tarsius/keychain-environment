7c08e8c4c3ea4d6eaee12d710a56793771f837c5
Jonas Bernoulli <jonas@bernoul.li>
2017-01-18 15:26:26 +0100

    support csh and fish shells

--- keychain-environment.el
+++ keychain-environment.el
@@ -60,13 +60,13 @@ information retrieved from files created by the keychain script."
   (let* ((ssh (shell-command-to-string "keychain -q --noask --agents ssh --eval"))
          (gpg (shell-command-to-string "keychain -q --noask --agents gpg --eval")))
     (list (and ssh
-               (string-match "SSH_AUTH_SOCK=\\(.*?\\);" ssh)
+               (string-match "SSH_AUTH_SOCK[=\s]\\([^\s;\n]*\\)" ssh)
                (setenv       "SSH_AUTH_SOCK" (match-string 1 ssh)))
           (and ssh
-               (string-match "SSH_AGENT_PID=\\([0-9]*\\)?;" ssh)
+               (string-match "SSH_AGENT_PID[=\s]\\([0-9]*\\)?" ssh)
                (setenv       "SSH_AGENT_PID" (match-string 1 ssh)))
           (and gpg
-               (string-match "GPG_AGENT_INFO=\\(.*?\\);" gpg)
+               (string-match "GPG_AGENT_INFO[=\s]\\([^\s;\n]*\\)" gpg)
                (setenv       "GPG_AGENT_INFO" (match-string 1 gpg))))))
 
 (provide 'keychain-environment)
